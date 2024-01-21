b9ab63043319835e36e6f9f822166b5132a2182d
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-21 23:07:52 +0800

    Done defining the function to remove chars up to number

--- Remove_First_Num_Characters.py
+++ Remove_First_Num_Characters.py
@@ -1,4 +1,10 @@
 # Remove first n characters from a string
+
 # pseudocode
+
 # Define the function to remove chars up to number 
+def remove_chars_up_to_number(input_str, number):
+    return input_str[number:]
+
+
 # Create an example and print it
