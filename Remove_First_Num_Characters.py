a1ea15a0f9d12baa896e9ac05a787783b500547c
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-21 23:15:51 +0800

    Done creating an example and printing it

--- Remove_First_Num_Characters.py
+++ Remove_First_Num_Characters.py
@@ -6,5 +6,7 @@
 def remove_chars_up_to_number(input_str, number):
     return input_str[number:]
 
-
 # Create an example and print it
+original_string="Hindi ko na kaya ko to!"
+result_string=remove_chars_up_to_number(original_string, 12)
+print(result_string)
